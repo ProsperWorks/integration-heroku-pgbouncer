36bea6632334623f1ec04e01301174caa62731d6
fzhang0111 <77305664+fzhang0111@users.noreply.github.com>
2021-05-29 10:26:16 -0400

    Update gen-pgbouncer-conf.sh

--- bin/gen-pgbouncer-conf.sh
+++ bin/gen-pgbouncer-conf.sh
@@ -5,11 +5,6 @@ POOL_MODE=${PGBOUNCER_POOL_MODE:-transaction}
 SERVER_RESET_QUERY=${PGBOUNCER_SERVER_RESET_QUERY}
 n=1
 
-# add TLS configuration: for client side of pgbouncer will be "require", for server side of pgbouncer will be "verify-ca". 
-# we need root.crt , certificate and key of pgbouncer on place for verifying, for handling secrets in security compliant manner 
-# they will be encrypted and come from SEO
-
-
 # if the SERVER_RESET_QUERY and pool mode is session, pgbouncer recommends DISCARD ALL be the default
 # http://pgbouncer.projects.pgfoundry.org/doc/faq.html#_what_should_my_server_reset_query_be
 if [ -z "${SERVER_RESET_QUERY}" ] &&  [ "$POOL_MODE" == "session" ]; then
@@ -76,9 +71,10 @@ tcp_keepidle = ${PGBOUNCER_TCP_KEEPIDLE:-7200}
 tcp_keepintvl = ${PGBOUNCER_TCP_KEEPINTVL:-75}
 EOFEOF
 
-# add certificates, key to pgbouncer.ini and change sslmode 
-# if all of certs and key are present and also adjust the certificates' formate 
-# from outputs of envrionment variables
+# add TLS configuration: for client side of pgbouncer will be "require", for server side of pgbouncer will be "verify-ca". 
+# we need root.crt , certificate and key of pgbouncer on place to verify, in order to handle secrets in security compliant manner 
+# they will be encrypted and come from SEO;
+# add certificates, key and change sslmode of pgbouncer.ini if all of certs and key are presented
 #
 if [ -n "${INTERNAL_TLS_CRT}" ] && [ -n "${INTERNAL_TLS_KEY}" ] && [ -n "${INTERNAL_CA_CRT}" ]
 then 
